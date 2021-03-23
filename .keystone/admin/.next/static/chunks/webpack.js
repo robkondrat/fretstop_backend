339ed1767777e385d0fdddd548f70406f2d63887
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-23 17:37:41 -0500

    created users

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6df30f7562bf5fee5f54";
+/******/ 	var hotCurrentHash = "0c57eb457efdbd7e0fce";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- .keystone/admin/pages/_app.js
+++ .keystone/admin/pages/_app.js
@@ -4,9 +4,13 @@ import { KeystoneProvider } from '@keystone-next/admin-ui/context';
 import { ErrorBoundary } from '@keystone-next/admin-ui/components';
 import { Core } from '@keystone-ui/core';
 
+import * as viewae69386b from '../../../node_modules/@keystone-next/fields/types/mongoId/views';
+import * as viewfcd710c6 from '../../../node_modules/@keystone-next/fields/types/text/views';
+import * as view1845079f from '../../../node_modules/@keystone-next/fields/types/password/views';
+
 const adminConfig = {};
 
-const fieldViews = {};
+const fieldViews = { viewae69386b, viewfcd710c6, view1845079f };
 
 const lazyMetadataQuery = {
   kind: 'Document',
@@ -157,7 +161,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="cgdttq"
+        adminMetaHash="3udl07"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
