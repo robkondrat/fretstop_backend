88c62b2e805ef790ec99185750abe04ccd163659
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-25 11:52:49 -0500

    updated queries and mutations

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cbc622763844e33ebc54";
+/******/ 	var hotCurrentHash = "d584b52e975c27dcf246";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
