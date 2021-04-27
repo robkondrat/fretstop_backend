3f092a7ff6b76dba1748aef966830e89695d309e
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-04-27 09:32:28 -0400

    including keystone files for prod

---
+++ .keystone/admin/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cart-items":["static\u002Fchunks\u002Fpages\u002Fcart-items.js"],"/guitar-images":["static\u002Fchunks\u002Fpages\u002Fguitar-images.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcart-items","\u002Fguitar-images"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
